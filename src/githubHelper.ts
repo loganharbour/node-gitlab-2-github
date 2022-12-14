9098c5e4a834f6f876951712b0aaebf43ad7fe19
Logan Harbour <loganhharbour@gmail.com>
2022-12-14 07:54:59 -0700

    fixup! 7a4ac37c25397ed08e38458d1104dfa4699dafaf

--- src/githubHelper.ts
+++ src/githubHelper.ts
@@ -1319,7 +1319,7 @@ export class GithubHelper {
       dateformatOptions
     );
 
-    const web_url = item.web_url
+    const web_url = new String(item.web_url)
     const web_url_replaced = web_url.replace('gitlab', 'hpcgitlab').replace('software', 'hpc').replace('idaholab', 'ncrc-archive')
     const attribution = `In GitLab by @${item.author.username} on ${formattedDate} ${web_url_replaced}`;
     const lineRef =
